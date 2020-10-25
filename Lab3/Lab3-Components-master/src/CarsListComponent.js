b60ceca1df3470efb094835bf20f3999ecd5da78
JacekCzupyt <kuba.czupyt@gmail.com>
2020-10-25 19:22:22 +0100

    Lab3Ex6

--- Lab3/Lab3-Components-master/src/CarsListComponent.js
+++ Lab3/Lab3-Components-master/src/CarsListComponent.js
@@ -7,16 +7,24 @@ class CarsListComponent extends React.Component{
         super(props);
         this.state = {
             cars: carsData.map((car) => 
-                <CarListItem carName={car.name} seats={car.seats} doors={car.doors} pricePerDay={car.pricePerDay} />)
+                <CarListItem carName={car.name} seats={car.seats} doors={car.doors} pricePerDay={car.pricePerDay} />),
+            searchTerm: ""
         }
+
+        this.SearchTermChangeHandle = this.SearchTermChangeHandle.bind(this);
+    }
+
+    SearchTermChangeHandle(e){
+        this.setState({searchTerm: e.target.value});
     }
 
     render(){
 
         return(
             <div>
-                {this.state.cars}
-            </div>
+                <input type="text" value={this.state.searchTerm} onChange={this.SearchTermChangeHandle}></input>
+                {this.state.cars.filter(car => {return ('' + car.props.carName).toLocaleLowerCase().includes(this.state.searchTerm.toLocaleLowerCase())})}
+            </div>//Please tell me there is a better way to cast a prop to string then that, because I couldn't find one for 30 min
         )
     }
 }
