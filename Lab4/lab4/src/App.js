cc755ad7c7b58fec37aaa2993e548eb661a94b95
JacekCzupyt <kuba.czupyt@gmail.com>
2020-10-27 08:55:08 +0100

    Lab4Ex1

---
+++ Lab4/lab4/src/App.js
@@ -0,0 +1,12 @@
+import CustomerForm from './CustomerForm'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerForm/>
+    </div>
+  );
+}
+
+export default App;
