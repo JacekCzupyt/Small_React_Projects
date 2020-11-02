55df85324c69d52dfc0b04a856186d9ab6eee771
JacekCzupyt <kuba.czupyt@gmail.com>
2020-11-02 15:59:56 +0100

    Lab4Ex3.1

--- Lab4/lab4/src/FormSteps/NameStep.js
+++ Lab4/lab4/src/FormSteps/NameStep.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function NameStep(props){
+
+    const [FirstName, SetFirstName] = useState("");
+    const [LastName, SetLastName] = useState("");
+    const [Email, SetEmail] = useState("");
+    const [ErrorText, SetErrorText] = useState("");
+
+    function InputValueChanged(e, setState){
+        setState(e.target.value);
+    }
+
+    function NextButtonPressed(){
+        //validate inputs
+        if(FirstName==""){
+            SetErrorText("First name must be provided");
+            return;
+        }
+        if(LastName==""){
+            SetErrorText("Last name must be provided");
+            return;
+        }
+        if(Email==""){
+            SetErrorText("Email must be provided");
+            return;
+        }
+        
+        props.MoveNextHook();
+    }
+
     return(
         <div>
-            NameStep
-            <button onClick={() => props.MoveNextHook()}>Next</button>
+            <input type="text" value={FirstName} placeholder="First Name" onChange={(e) => InputValueChanged(e, SetFirstName)}></input>
+            <br/>
+            <input type="text" value={LastName} placeholder="Last Name" onChange={(e) => InputValueChanged(e, SetLastName)}></input>
+            <br/>
+            <input type="text" value={Email} placeholder="Email" onChange={(e) => InputValueChanged(e, SetEmail)}></input>
+            <br/>
+            <button onClick={() => NextButtonPressed()}>Next</button>
+            <label style={{color: "red"}}>{ErrorText}</label>
         </div>
     )
 }
