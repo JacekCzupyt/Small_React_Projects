5d048e64a33933293964ee3e77b9cfe429f4b8f9
JacekCzupyt <kuba.czupyt@gmail.com>
2020-11-14 16:28:12 +0100

    Lab6Ex3

--- Lab6/lab6/src/GameBoard.jsx
+++ Lab6/lab6/src/GameBoard.jsx
@@ -5,7 +5,8 @@ import GameTile from './GameTile'
 
 const mapStateToProps = (state) =>{
     return{
-        BoardSize: state.BoardSize//temp
+        BoardSize: state.BoardSize,
+        BoardState: state.BoardState
     }
 }
 
@@ -15,10 +16,17 @@ const mapDispatchToProps = (dispatch) => ({
 
 
 const GameBoard = (props) =>{
+    const Board = Array(props.BoardSize).fill(0).map((val1, ind1) => 
+        //There was a white space in between the divs (rows) no matter what I did, this was the only solution I found
+        <div style={{marginBottom:"-4px"}}>
+            {Array(props.BoardSize).fill(0).map((val2, ind2) => <GameTile highlighted={props.BoardState[ind1][ind2]}/>)}
+        </div>
+    )
+
+
     return(
         <div>
-            <GameTile highlighted={true}/>
-            <GameTile highlighted={false}/>
+            {Board}
         </div>
     )
 }
