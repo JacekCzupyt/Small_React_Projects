d9cbe7a27df3a022a4d5c53d7a5ebc59af6b995f
JacekCzupyt <kuba.czupyt@gmail.com>
2020-11-30 22:57:11 +0100

    Lab8Ex4

--- Lab8/lab8/src/Component.jsx
+++ Lab8/lab8/src/Component.jsx
@@ -13,9 +13,24 @@ function Component(porps){
 
     const LoadData = () => {
         setIsLoaded(false);
-        fetch("http://localhost:3004/employees").then(res => res.json())
+        fetch("http://localhost:3004/employees")
+            .then(res => res.json())
             .then(data => setEmployees(data))
             .then(() => setIsLoaded(true))
+            .catch(error => console.error(error));
+    }
+
+    const DeleteItem = (e) => {
+        let emps = employees;
+        setEmployees(
+            emps.map(
+                (emp) => emp.id == e.target.name ? 
+                "" :
+                emp));
+
+
+        fetch("http://localhost:3004/employees/" + e.target.name, {method: 'DELETE'})
+            .then(() => LoadData())
             .catch(error => console.error(error));
     }
 
@@ -27,7 +42,10 @@ function Component(porps){
         <div>
             {isLoaded ? 
             <div>
-                {employees.map((employee) => (<div><p>{employee.id} : {employee.name}</p></div>))}
+                {employees.map((employee) => employee == "" ? 
+                    (<div>"Removing..."</div>) : 
+                    (<div><p>{employee.id} : {employee.name}    <button name={employee.id} onClick={DeleteItem}>Delete</button></p></div>))
+                }
                 <Switch>
                     <Route exact path="/">
                         <button onClick={() => history.push('/add')}>Add Employee</button> 
